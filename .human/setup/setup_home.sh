93a9566c7c407649bba649daea21ba28bd080574
Stefan Dunca <47554641+stefandunca@users.noreply.github.com>
2021-05-07 19:41:40 +0200

    Simplify docs

--- .human/setup/setup_home.sh
+++ .human/setup/setup_home.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Install ansible
+echo "Install required dependencies"
+if ! command -v pacman &> /dev/null
+then
+    sudo pacman -Syu git ansible openssh
+else
+    sudo apt update
+    sudo apt install -y software-properties-common ansible git
+fi
+
+# Manual entry: `alias home='git --work-tree=$HOME --git-dir=$HOME/.home'`
 # Create an alias command for git commands with set work dir to home and ".git" dir as ".home"
 home() {
     git --work-tree=$HOME --git-dir=$HOME/.home $*
@@ -9,9 +20,11 @@ export -f home
 if [ ! -d "$HOME/.home" ]; then
     echo "Cloning home-setup"
 
+    # Ensure the main branch is right
+    git config --global init.defaultBranch main
     # Bring saved configuration files from the repository
     home init && home remote add origin https://github.com/stefandunca/home-config.git
-    home fetch && home checkout master && home submodule update --init --recursive
+    home fetch && home checkout main && home submodule update --init --recursive
     # Trigger customized configuration on bash interactive startup
     echo "Add home-setup scripts at bash login"
     if [[ -z "$ZSH_VERSION" ]]; then
